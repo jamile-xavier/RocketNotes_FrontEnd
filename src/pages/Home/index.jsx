c2c55ef3867385759f640bfa0e5a0d2a9126e755
Jamile-Xavier <jamile.x.m@gmail.com>
2023-07-18 18:37:38 -0300

    início da página home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,36 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
+import { FiPlus } from "react-icons/fi";
+
+import { Header } from "../../components/Header";
+import { ButtonText } from "../../components/ButtonText";
+
+export function Home() {
+  return (
+    <Container>
+      <Brand>
+        <h1>Rocketnotes</h1>
+      </Brand>
+      <Header />
+      <Menu>
+        <li>
+          <ButtonText title="Todos" isActive />
+        </li>
+        <li>
+          <ButtonText title="Frontend" />
+        </li>
+        <li>
+          <ButtonText title="Node" />
+        </li>
+        <li>
+          <ButtonText title="React" />
+        </li>
+      </Menu>
+      <Search></Search>
+      <Content></Content>
+      <NewNote>
+        <FiPlus />
+        Criar nota
+      </NewNote>
+    </Container>
+  );
+}
